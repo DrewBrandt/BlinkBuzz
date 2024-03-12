d9eee067927f0aefe31f8d0e55d482ee01db2c73
Drew Brandt <ahbrandt@umd.edu>
2024-03-12 12:46:16 -0400

    More reorginization

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,34 +1,53 @@
 
 
-#include "BlinkBuzz.h"
+#include "../src/BlinkBuzz.h"
 
-int pins[] = { 13, 33 };
+#define BUZZER 33
+
+int pins[] = {13, BUZZER};
 BlinkBuzz bb = BlinkBuzz(pins, 2, true);
-void loop(int);
 
+#ifndef ARDUINO
+void loop(int = 0);
+void setup();
 int main()
 {
-	int start = millis();
-	bb.aonoff(13, 200, 3, 1000);
-	loop(start);
+	setup();
+	loop(millis());
+}
+#endif // !ARDUINO
+
+void setup()
+{
+	bb.aonoff(13, 200, 3, 1000); // blink pin 13 3 times, 200ms on, 1000ms off asynchronously
 }
 
-void loop(int start) {
+void loop(int start)
+{
 	int time = 0, last = 0;
 	bool passed1500 = false;
-	while (true) {
+	while (true)
+	{
 		time = millis();
 		bb.update();
-		if (time - last > 100) {
+		if (time - last > 100)
+		{
 			last = time;
-			if (!passed1500 && time - start > 1500) {
+			if (!passed1500 && time - start > 1500)
+			{ // turn on pin 33 after 1.5 seconds, for 1 second
 				passed1500 = true;
-				//bb.aonoff(33, 1000);
+				bb.aonoff(BUZZER, 1000);
 			}
-			printf("%i.%03i between\n", time / 1000, time % 1000);
-			if (time - start > 5000) {
+
+#ifndef ARDUINO
+			printf("%i.%03i between\n", time / 1000, time % 1000); // demonstrate asynchronous blinking
+#else
+			char buf[50];
+			snprintf(buf, 50, "%i.%03i between\n", time / 1000, time % 1000);
+			Serial.println(buf);
+#endif
+			if (time - start > 5000)
 				break;
-			}
 		}
 	}
 }
